2a47f53984330e5ba1c7b58efe75167ae6b88c62
NikosBelogiannis <markus.einspanier@gmail.com>
2019-01-11 08:19:31 +0100

    Alles gut!

--- src/model/weapons/weapon.java
+++ src/model/weapons/weapon.java
@@ -1,5 +1,61 @@
 package model.weapons;
 
-public abstract class weapon {
+import model.Body;
 
+public abstract class Weapon {
+
+    private Body wBody;
+    private int wHp;
+    private int wStamina;
+    private int wSpeed;
+    private int wStrength;
+    private int wResistance;
+
+    public Body getwBody() {
+        return wBody;
+    }
+
+    public void setwBody(Body wBody) {
+        this.wBody = wBody;
+    }
+
+    public int getwHp() {
+        return wHp;
+    }
+
+    public void setwHp(int wHp) {
+        this.wHp = wHp;
+    }
+
+    public int getwStamina() {
+        return wStamina;
+    }
+
+    public void setwStamina(int wStamina) {
+        this.wStamina = wStamina;
+    }
+
+    public int getwSpeed() {
+        return wSpeed;
+    }
+
+    public void setwSpeed(int wSpeed) {
+        this.wSpeed = wSpeed;
+    }
+
+    public int getwStrength() {
+        return wStrength;
+    }
+
+    public void setwStrength(int wStrength) {
+        this.wStrength = wStrength;
+    }
+
+    public int getwResistance() {
+        return wResistance;
+    }
+
+    public void setwResistance(int wResistance) {
+        this.wResistance = wResistance;
+    }
 }
