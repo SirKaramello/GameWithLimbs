5465ac294d605bf0a65e76b1f2f4dc3074495b2b
SirKaramello <43502063+SirKaramello@users.noreply.github.com>
2019-01-28 05:41:31 +0100

    Kommentare

--- src/model/powerUP/PowerUP.java
+++ src/model/powerUP/PowerUP.java
@@ -9,74 +9,80 @@ import java.awt.image.BufferedImage;
 /** nicht in dieser klasse weiterarbeiten, es wird umverlagert zu Items */
 
 public abstract class PowerUP extends GraphicalObject {
-
-    private Body uBody;
-    private int uHp;
-    private int uStamina;
-    protected int powerUpType;
-    private int uSpeed;
-    private int uStrength;
-    private int uResistance;
-    protected double TimeStopCount;
-    protected int amountOfST;
-    protected boolean STOP;
+    //Attribute
+    private int uStamina,uResistance,amountOfST,uSpeed,uStrength,uHp,powerUpType;
+    protected double TimeStopCount,yMax;
+    protected boolean STOP,pickedUp;
     protected String filePath;
-    protected boolean pickedUp;
-    protected double yMax ;
+    //Referenzen
+    private Body uBody;
 
+    /**
+     * @return gibt den FilePath des Bildes zurücl
+     */
     public String getFilePath() {
         return filePath;
     }
 
-
-    public Body getuBody() {
-        return uBody;
-    }
-
+    /*
+     * @return gibt die neue hp vom Spieler zurücl
+     */
     public int getuHp() {
         return uHp;
     }
-
+    /*
+     * @return gibt die neue stamina vom Spieler zurücl
+     */
     public int getuStamina() {
         return uStamina;
     }
-
+    /*
+     * @return gibt die neue speed vom Spieler zurücl
+     */
     public int getuSpeed() {
         return uSpeed;
     }
-
+    /*
+     * setzt den neuen Spieler
+     */
     public void setuBody(Body uBody) {
         this.uBody = uBody;
     }
 
-    public void setuHp(int uHp) {
-        this.uHp = uHp;
-    }
-
-    public void setuStamina(int uStamina) {
-        this.uStamina = uStamina;
-    }
-
+    /*
+     * setzt die neue speed
+     */
     public void setuSpeed(int uSpeed){
         this.uSpeed = uSpeed;
     }
-
+    /*
+    * @return die neue stärle
+    */
     public int getuStrength() {
         return uStrength;
     }
-
+    /*
+    * setzt die neue stärle
+     */
     public void setuStrength(int uStrength) {
         this.uStrength = uStrength;
     }
-
+    /*
+       * @return die neue resistenz
+       */
     public int getuResistance() {
         return uResistance;
     }
-
+    /*
+       * setzt die resistenz
+       */
     public void setuResistance(int uResistance) {
         this.uResistance = uResistance;
     }
 
+    /*
+     * @return gibt den Typ des Powerups zurück
+     */
     public int getPowerUpType() {
         return powerUpType;
     }
@@ -85,24 +91,9 @@ public abstract class PowerUP extends GraphicalObject {
         this.powerUpType = powerUpType;
     }
 
-    public double getTimeStopCount() {
-        return TimeStopCount;
-    }
-
-    public void updateTimeStopCount(PowerUP p) {
-        TimeStopCount = p.getAmountOfST();
-    }
-
-    public int getAmountOfST() {
-        return amountOfST;
-    }
 
     public void setAmountOfST(int amountOfST) {
         this.amountOfST = amountOfST;
-    }
-
-    public boolean isSTOP() {
-        return STOP;
     }
 
     public void setSTOP(boolean STOP) {
@@ -113,26 +104,44 @@ public abstract class PowerUP extends GraphicalObject {
         TimeStopCount = timeStopCount;
     }
 
+    /**
+     * Bewegung
+     * @param dt vrstrichene Zeit seit dem letzten Aufruf der MEthode
+     */
     public void update(double dt){
         if(y <= yMax){
             y += Math.random()*500*dt;
         }
     }
 
+    /**
+     * Lässt die x koordinate random umändern
+     */
     public void jump(){
         x=Math.random()*1800;
     }
 
+    /**
+     * zeicgnet die powerups
+     * @param drawTool das werkzeug
+     */
     public void draw(DrawTool drawTool){
         if(!pickedUp && uBody!=null && uBody.getBg()!=null &&uBody.getBg().getMode().equals("fight")){
             drawTool.drawImage(this.getMyImage(), x, y);
         }
     }
 
+    /**
+     * setzt pickedUp
+     * @param p der neue wahrheitswert
+     */
     public void setPickedUp(boolean p){
         pickedUp=p;
     }
 
+    /**
+     * @return pickedUp
+     */
     public boolean getPickedUp(){
         return pickedUp;
     }
